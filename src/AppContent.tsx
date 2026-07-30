86497bd75dd3afcd803fefbf02ce0f74bfadb178
Nachiket Galande <nachiketgalande1609@gmail.com>
2026-07-31 02:31:09 +0530

    UI fixes

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -464,7 +464,7 @@ const AppContent = () => {
           padding: 0,
           margin: 0,
           width: "100%",
-          pt: isPublicRoute ? 0 : { xs: "52px", sm: 0 },
+          pt: isPublicRoute || location.pathname.startsWith("/messages") ? 0 : { xs: "52px", sm: 0 },
         }}
       >
         <Routes>
